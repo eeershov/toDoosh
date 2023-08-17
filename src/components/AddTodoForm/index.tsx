3ce633775cc465730993d5bdde61d385e8decb37
Evgeniy Ershov <ershov.ee@gmail.com>
2023-08-17 19:34:01 +0300

    refactor prettier AddTodoForm

--- src/components/AddTodoForm/index.tsx
+++ src/components/AddTodoForm/index.tsx
@@ -15,16 +15,16 @@ const AddTodoForm = () => {
 	};
 
 	return (
-		<form onSubmit={onSubmit} className='bg-gray-200 align-middle'>
+		<form onSubmit={onSubmit} className='items-center flex'>
 			<input
 				type='text'
-				className='rounded-xl h-8 mx-2 my-2 p-2'
+				className='rounded-xl h-6 mx-2 my-2 p-4 ring-2 max-w-xl ring-purple-400 w-full'
 				placeholder='Add todo'
 				value={value}
 				onChange={(event) => setValue(event.target.value)}
 			></input>
 
-			<button type='submit' className=''>
+			<button type='submit' className='rounded-full bg-purple-400 hover:bg-purple-600 px-2 py-1 h-8 mx-2'>
 				Submit
 			</button>
 		</form>
