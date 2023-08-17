534060ddd608f257714391e5c1e6d08864810756
Evgeniy Ershov <ershov.ee@gmail.com>
2023-08-17 17:30:53 +0300

    feature working filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
 import AddTodoForm from './components/AddTodoForm';
 import { FiltersBar } from './components/FiltersBar';
+import { Todo } from './interfaces/todo';
+import { Filter, filters } from './interfaces/filters';
 import { useAppSelector, useAppDispatch } from './redux/hooks';
 import { getTodosAsync } from './redux/todoSlice';
 import StatusHandler from './components/FetchStatusHandler';
 
 function App() {
+  const [activeTodos, setActiveTodos] = useState([]);
   const {todos, status, activeFilter} = useAppSelector(state => state.todos);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getTodosAsync());
   }, [dispatch]);
+
+  const filterTodos = (todos: Todo[], activeFilter: Filter) => {
+    switch (activeFilter) {
+      case (filters.active):
+        return todos.filter(todo => todo.completed === false);
+      case (filters.completed):
+        return todos.filter(todo => todo.completed === true);
+      default:
+        return todos;
+    }
+  };
+  useEffect(() => {
+    setActiveTodos(filterTodos(todos, activeFilter));
+  }, [todos, activeFilter]);
 
   return (
     <div className='bg-gray-200'>
@@ -22,7 +39,7 @@ function App() {
         <FiltersBar activeFilter={activeFilter}/>
         <AddTodoForm/>
         <StatusHandler status={status}/>
-        <TodoList {...{ todos: todos }}/>
+        <TodoList {...{ todos: activeTodos }}/>
       </div>
     </div>
   );
